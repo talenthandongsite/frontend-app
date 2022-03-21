3c95feaa4659082e384bb0f0c12d85a827e5e636
cocm1324 <cocm1324@gmail.com>
2022-03-21 19:18:59 +0900

    deploy script fix1

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -8,4 +8,4 @@ docker build -f deployments/dockerfile -t ${NAME}:${TAG} .
 docker ps -f name=${NAME} -q | xargs --no-run-if-empty docker container stop
 docker container ls -a -fname=${NAME} -q | xargs -r docker container rm
 docker images --no-trunc --all --quiet --filter='dangling=true' | xargs --no-run-if-empty docker rmi
-docker run -p ${HTTP} -p ${HTTP} --network ${DOCKER_NETWORK} --add-host host.docker.internal:host-gateway -d --name ${NAME} ${NAME}:${TAG}
+docker run -p ${HTTP} -p ${HTTPS} --network ${DOCKER_NETWORK} --add-host host.docker.internal:host-gateway -d --name ${NAME} ${NAME}:${TAG}
